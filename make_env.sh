12b806dd6263f5300417225de0ff72e2a0ec8102
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-11-05 13:23:35 +1100

    adjust env creation steps

--- make_env.sh
+++ make_env.sh
@@ -1,8 +1,9 @@
 conda create -n multipoint python=3.6 bilby bilby_pipe gwpy
 conda activate multipoint
+pip uninstall dynesty
 cd ..
 git clone https://github.com/avivajpeyi/dynesty
 cd dynesty
 git checkout -b refactor_rwalk_to_return_more_points origin/refactor_rwalk_to_return_more_points
-python setup.py install
+python setup.py develop
 
