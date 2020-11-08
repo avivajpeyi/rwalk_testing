bebcbfff7a52af75dadd41d8714de3c6ae927a85
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-11-09 09:47:06 +1100

    update paths

--- results/plotter.py
+++ results/plotter.py
@@ -1,5 +1,6 @@
-import bilby, glob
+import bilby
 import corner
+import glob
 import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,7 +28,6 @@ def overlaid_corner(samples_list, sample_labels, param=[], fname="test.png"):
     max_len = max([len(s) for s in samples_list])
     cmap = plt.cm.get_cmap('gist_rainbow', n)
     colors = [cmap(i) for i in range(n)]
-
 
     fig = corner.corner(
         samples_list[0],
@@ -60,24 +60,23 @@ def get_normalisation_weight(len_current_samples, len_of_longest_samples):
     return np.ones(len_current_samples) * (len_of_longest_samples / len_current_samples)
 
 
-
 RES = {
     'multidimensional_gaussian.py': [
-     '../tests/outdir_regular_rwalk_multidimensional_gaussian.py/regular_rwalk_multidimensional_gaussian.py_result.json',
-     '../tests/outdir_multi_rwalk_multidimensional_gaussian.py/multi_rwalk_multidimensional_gaussian.py_result.json',   
-     ],
-     'bns.py': [
-     '../tests/outdir_regular_rwalk_bns.py/regular_rwalk_bns.py_result.json',
-     '../tests/outdir_multi_rwalk_bns.py/multi_rwalk_bns.py_result.json'
-     ],
-     'fast_tutorial.py': [
-     '../tests/outdir_regular_rwalk_fast_tutorial.py/regular_rwalk_fast_tutorial.py_result.json',
-     '../tests/outdir_multi_rwalk_fast_tutorial.py/multi_rwalk_fast_tutorial.py_result.json'
-     ],
-     'gw150914.py': [
-     '../tests/outdir_regular_rwalk_gw150914.py/regular_rwalk_gw150914.py_result.json',
-     '../tests/outdir_multi_rwalk_gw150914.py/multi_rwalk_gw150914.py_result.json'
-     ]
+        '../tests/outdir_regular_rwalk_multidimensional_gaussian.py/regular_rwalk_multidimensional_gaussian_result.json',
+        '../tests/outdir_multi_rwalk_multidimensional_gaussian.py/multi_rwalk_multidimensional_gaussian_result.json',
+    ],
+    'bns.py': [
+        '../tests/outdir_regular_rwalk_bns.py/regular_rwalk_bns_result.json',
+        '../tests/outdir_multi_rwalk_bns.py/multi_rwalk_bns_result.json'
+    ],
+    'fast_tutorial.py': [
+        '../tests/outdir_regular_rwalk_fast_tutorial.py/regular_rwalk_fast_tutorial_result.json',
+        '../tests/outdir_multi_rwalk_fast_tutorial.py/multi_rwalk_fast_tutorial_result.json'
+    ],
+    'gw150914.py': [
+        '../tests/outdir_regular_rwalk_gw150914.py/regular_rwalk_gw150914_result.json',
+        '../tests/outdir_multi_rwalk_gw150914.py/multi_rwalk_gw150914_result.json'
+    ]
 }
 
 
@@ -90,96 +89,101 @@ def get_result_paths():
     ]
     results = glob.glob("tests/out*/*result.json")
 
-    test_result = {k:[] for k in TESTS}
+    test_result = {k: [] for k in TESTS}
     for t in TESTS:
         for r in results:
             if t in r:
                 test_result[t].append(r)
     return test_result
 
-def print_info(normal,multi, fname):
-    with open(fname, "w") as f:
-        f.write(f"Normal:\n{(normal.sampling_time)/(60*60):.2f}hr\n{normal}")
-        f.write(f"Multi:\n{(multi.sampling_time)/(60*60):.2f}hr\n{multi}")
 
-    
+def print_info(normal, multi, fname):
+    with open(fname, "w") as f:
+        f.write(f"Normal:\n{(normal.sampling_time) / (60 * 60):.2f}hr\n{normal}")
+        f.write(f"Multi:\n{(multi.sampling_time) / (60 * 60):.2f}hr\n{multi}")
+
+
 def multid():
     r = RES["multidimensional_gaussian.py"]
     normal = bilby.gw.result.CBCResult.from_json(r[0])
     multi = bilby.gw.result.CBCResult.from_json(r[1])
-    samples_list=[normal.posterior, multi.posterior]
-    param = [f"mu_{i}" for i in range(5)] + [f"sigma_{i}" for i in range(5)] + ["log_likelihood"]
+    samples_list = [normal.posterior, multi.posterior]
+    param = [f"mu_{i}" for i in range(5)] + [f"sigma_{i}" for i in range(5)] + [
+        "log_likelihood"]
     overlaid_corner(
-            samples_list=[s[param] for s in samples_list],
-            sample_labels=["normal","multi"],
-            fname="multidim_corner.png"
+        samples_list=[s[param] for s in samples_list],
+        sample_labels=["normal", "multi"],
+        fname="multidim_corner.png"
     )
-    print_info(normal,multi, "multidim_stats.txt")
-    
-    
+    print_info(normal, multi, "multidim_stats.txt")
+
+
 def fast_tut():
     r = RES["fast_tutorial.py"]
     normal = bilby.gw.result.CBCResult.from_json(r[0])
     multi = bilby.gw.result.CBCResult.from_json(r[1])
-    samples_list=[normal.posterior, multi.posterior]
-    param=['mass_ratio', 'chirp_mass', 'luminosity_distance', 'theta_jn']+ ["log_likelihood"]
+    samples_list = [normal.posterior, multi.posterior]
+    param = ['mass_ratio', 'chirp_mass', 'luminosity_distance', 'theta_jn'] + [
+        "log_likelihood"]
     overlaid_corner(
-            samples_list=[s[param] for s in samples_list],
-            sample_labels=["normal","multi"],
-            fname="fast_corner.png"
+        samples_list=[s[param] for s in samples_list],
+        sample_labels=["normal", "multi"],
+        fname="fast_corner.png"
     )
-    print_info(normal,multi, "fast_stats.txt")
-    
+    print_info(normal, multi, "fast_stats.txt")
 
-    
+
 def gw150914():
     r = RES["gw150914.py"]
     normal = bilby.gw.result.CBCResult.from_json(r[0])
     multi = bilby.gw.result.CBCResult.from_json(r[1])
-    samples_list=[normal.posterior, multi.posterior]
-    param= ["mass_ratio",
-            "chirp_mass",
-            "mass_1",
-            "mass_2",
-            "a_1",
-            "a_2",
-            "tilt_1",
-            "tilt_2",
-            "phi_12",
-            "phi_jl",
-            "luminosity_distance",
-            "dec",
-            "ra",
-            "theta_jn",
-            "psi",
-            "phase",
-            "geocent_time"] + ["log_likelihood"]
+    samples_list = [normal.posterior, multi.posterior]
+    param = ["mass_ratio",
+             "chirp_mass",
+             "mass_1",
+             "mass_2",
+             "a_1",
+             "a_2",
+             "tilt_1",
+             "tilt_2",
+             "phi_12",
+             "phi_jl",
+             "luminosity_distance",
+             "dec",
+             "ra",
+             "theta_jn",
+             "psi",
+             "phase",
+             "geocent_time"] + ["log_likelihood"]
     overlaid_corner(
-            samples_list=[s[param] for s in samples_list],
-            sample_labels=["normal","multi"],
-            fname="gw150914_corner.png"
+        samples_list=[s[param] for s in samples_list],
+        sample_labels=["normal", "multi"],
+        fname="gw150914_corner.png"
     )
-    print_info(normal,multi, "gw150914_stats.txt")
-    
+    print_info(normal, multi, "gw150914_stats.txt")
+
 
 def bns():
     r = RES["bns.py"]
     normal = bilby.gw.result.CBCResult.from_json(r[0])
     multi = bilby.gw.result.CBCResult.from_json(r[1])
-    samples_list=[normal.posterior, multi.posterior]
-    param = ["chirp_mass", "symmetric_mass_ratio", "lambda_tilde", "delta_lambda"]+ ["log_likelihood"]
+    samples_list = [normal.posterior, multi.posterior]
+    param = ["chirp_mass", "symmetric_mass_ratio", "lambda_tilde", "delta_lambda"] + [
+        "log_likelihood"]
     overlaid_corner(
-            samples_list=[s[param] for s in samples_list],
-            sample_labels=["normal","multi"],
-            fname="bns_corner.png"
+        samples_list=[s[param] for s in samples_list],
+        sample_labels=["normal", "multi"],
+        fname="bns_corner.png"
     )
-    print_info(normal,multi, "bns_stats.txt")
-    
+    print_info(normal, multi, "bns_stats.txt")
+
+
 def main():
     fast_tut()
     bns()
     gw150914()
     multid()
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
