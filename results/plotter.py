36b86bdc489cd4849d9cd0e889ccdbe403d21dfa
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-11-12 21:40:50 +1100

    add colorblind colors

--- results/plotter.py
+++ results/plotter.py
@@ -6,6 +6,7 @@ import corner
 import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 CORNER_KWARGS = dict(
     smooth=0.9,
@@ -22,14 +23,29 @@ CORNER_KWARGS = dict(
 )
 
 
+from typing import List, Optional
+import seaborn as sns
+
+def get_colors(num_colors: int, alpha: Optional[float]=1) -> List[List[float]]:
+    """Get a list of colorblind colors,
+    :param num_colors: Number of colors.
+    :param alpha: The transparency
+    :return: List of colors. Each color is a list of [r, g, b, alpha].
+    """
+    cs = sns.color_palette(palette="colorblind", n_colors=num_colors)
+    cs = [list(c) for c in cs]
+    for i in range(len(cs)):
+        cs[i].append(alpha)
+    return cs
+
+
 def overlaid_corner(samples_list, sample_labels, param=[], fname="test.png"):
     """Plots multiple corners on top of each other"""
     print(f"Creating {fname}")
     n = len(samples_list)
 
     max_len = max([len(s) for s in samples_list])
-    cmap = plt.cm.get_cmap('gist_rainbow', n)
-    colors = [cmap(i) for i in range(n)]
+    colors = get_colors(len(samples_list))
 
     fig = corner.corner(
         samples_list[0],
