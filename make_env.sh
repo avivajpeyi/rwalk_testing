e1122a3b5cf4d5f87082a4d62b850a289d57b7f1
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-06-23 10:47:39 +1000

    Update make_env.sh

--- make_env.sh
+++ make_env.sh
@@ -9,7 +9,7 @@ python3 -m venv envs/main && source envs/main/bin/activate
 git clone https://github.com/joshspeagle/dynesty.git packages/main/dynesty_main
 cd packages/main/dynesty_main && python setup.py develop && cd -
 git clone git@git.ligo.org:lscsoft/bilby.git packages/main/bilby_main
-cd packages/main/bilby_main && pip install -r requirements.txt && python setup.py develop && pip install bilby_pipe && cd -
+cd packages/main/bilby_main && pip3 install -r requirements.txt && python setup.py develop && pip3 install bilby_pipe && cd -
 deactivate
 
 echo "creating venv with modded branch of software"
@@ -17,5 +17,5 @@ python3 -m venv envs/modded && source envs/modded/bin/activate
 git clone https://github.com/avivajpeyi/dynesty packages/modded/dynesty_modded
 cd packages/modded/dynesty_modded && git checkout -b refactor_rwalk_to_return_more_points origin/refactor_rwalk_to_return_more_points && python setup.py develop && cd -
 git clone git@git.ligo.org:avi.vajpeyi/bilby.git packages/modded/bilby_modded
-cd  packages/modded/bilby_modded && pip install -r requirements.txt && git checkout -b refactor_rwalk_to_return_more_points origin/refactor_rwalk_to_return_more_points && python setup.py develop && pip install bilby_pipe && cd -
+cd  packages/modded/bilby_modded && pip3 install -r requirements.txt && git checkout -b refactor_rwalk_to_return_more_points origin/refactor_rwalk_to_return_more_points && python setup.py develop && pip3 install bilby_pipe && cd -
 deactivate 
